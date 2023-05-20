48fcaa75a5575111c02e99169952abf844b1c9b9
Vladimir Looze <woldemar@mimas.ru>
2023-05-20 19:49:57 +0400

    Update runner_test.py

--- process_performance/runner_test.py
+++ process_performance/runner_test.py
@@ -32,7 +32,7 @@ class InvokeContextProcessTimes(InvokeContextInterface):
 
 
 def test_spawn_cpu_times():
-    _t = 3
+    _t = 1
     _times = '__import__(\'os\').times()'
     _args = {
         'cpu loader': f"while min({_times}.system, {_times}.user) < {_t}: pass"
